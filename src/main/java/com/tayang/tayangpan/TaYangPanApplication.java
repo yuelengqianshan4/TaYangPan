3741f72fbf491aa45969951f5b677061c60f5245
yuelengqianshan4 <yuelengqianshan4@163.com>
2023-06-13 09:16:12 +0800

    --

---
+++ src/main/java/com/tayang/tayangpan/TaYangPanApplication.java
@@ -0,0 +1,13 @@
+package com.tayang.tayangpan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaYangPanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaYangPanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tayang/tayangpan/TaYangPanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tayang.tayangpan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaYangPanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
